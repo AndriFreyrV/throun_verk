c10e734cbfd64f70d09debf9dd3e24002991d90c
AndriFV <afv6@hi.is>
2021-03-19 18:07:39 +0000

    init stuff

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,30 @@
 package sample;
 
-public class Controller {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private ComboBox<Integer> howMany;
+
+    private final Integer[] num_vals = {1,2,3,4,5,6,7,8,9,10};
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        addNums();
+    }
+
+    public void addNums(){
+        ObservableList<Integer> vals_ls =
+                FXCollections.observableArrayList(num_vals);
+        howMany.setItems(vals_ls);
+    }
 }
